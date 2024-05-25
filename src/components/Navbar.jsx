bf574827d245576eb10ca99e812110117a5fe3e0
Vicky-peace <victorpeace47@gmail.com>
2024-05-25 21:53:43 +0300

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom';
 import './navbar.css'
 import logo from '../assets/shared/logo.svg';
 
 const Navbar = () => {
+  const {isOpen, setIsOpen} =  useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  }
   return (
     <div className='navbar'>
        <div className="logo">
             <img src={logo} alt="Logo" />
        </div>
-       <div className="nav-container">
+       <div className={`nav-container  ${isOpen ? 'open' : ''}`}>
+        <button className="humberger" onClick={toggleNav}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </button>
            <ul>
               <li className="nav-item">
-               <NavLink to="/" className="nav-link">00 Home</NavLink>
+               <NavLink to="/" className="nav-link"onClick={toggleNav}>00 Home</NavLink>
               </li>
               <li className="nav-item">
-               <NavLink to="/dest" className="nav-link">01 Destination</NavLink>
+               <NavLink to="/dest" className="nav-link" onClick={toggleNav}>01 Destination</NavLink>
               </li>
               <li className="nav-item">
-               <NavLink to="/crew" className="nav-link">02 Crew</NavLink>
+               <NavLink to="/crew" className="nav-link" onClick={toggleNav}>02 Crew</NavLink>
               </li>
               <li className="nav-item">
-               <NavLink to="/tech" className="nav-link">03 Technology</NavLink>
+               <NavLink to="/tech" className="nav-link" onClick={toggleNav}>03 Technology</NavLink>
               </li>
             
            </ul>
